7b124924c48967d32cd1824682f652e09b7077f0
chao <rc1029@gmail.com>
2011-12-04 19:15:20 -0800

    Enable to upload two photos

--- src/com/qc/camera/CameraActivity.java
+++ src/com/qc/camera/CameraActivity.java
@@ -5,7 +5,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -22,97 +25,202 @@ import com.qc.R;
 import com.qc.Util;
 
 public class CameraActivity extends Activity {
-    // define the file-name to save photo taken by Camera activity
-    String fileName = "muzhigirl.jpg";
+	// define the file-name to save photo taken by Camera activity
+	String FIRST_FILE_NAME = "muzhigirl1.jpg";
+	String SECOND_FILE_NAME = "muzhigirl2.jpg";
 
-    // create parameters for Intent with filename
-    ContentValues values;
-    Button buttonTakePhoto;
-    Button buttonChoosePhoto;
-    ImageView imgTakenPhoto;
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
-    private static final int PICK_IMAGE_ACTIVITY_REQUESRT_CODE = 1;
+	// create parameters for Intent with filename
+	ContentValues values;
+	Button buttonFirstPhoto;
+	Button buttonSecondPhoto;
+	ImageView firstPhoto;
+	ImageView secondPhoto;
+	private static final int CAPTURE_FIRST_IMAGE_ACTIVITY_REQUEST_CODE = 0;
+	private static final int PICK_FIRST_IMAGE_ACTIVITY_REQUESRT_CODE = 1;
+	private static final int CAPTURE_SECOND_IMAGE_ACTIVITY_REQUEST_CODE = 2;
+	private static final int PICK_SECOND_IMAGE_ACTIVITY_REQUESRT_CODE = 3;
 
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        super.onPause();
-        Util.clearImageResource(imgTakenPhoto);
-    }
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Util.clearImageResource(firstPhoto);
+		Util.clearImageResource(secondPhoto);
+	}
 
-    @Override
-    protected void onDestroy() {
-        // TODO Auto-generated method stub
-        super.onDestroy();
-        Util.clearImageResource(imgTakenPhoto);
-    }
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Util.clearImageResource(firstPhoto);
+		Util.clearImageResource(secondPhoto);
+	}
 
-    class buttonChoosePhotoClicker implements Button.OnClickListener {
-        public void onClick(View v) {
-            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            photoPickerIntent.setType("image/*");
-            startActivityForResult(photoPickerIntent, PICK_IMAGE_ACTIVITY_REQUESRT_CODE);
-        }
-    }
+	class ButtonChoosePhotoClicker implements OnClickListener {
+		int whichphoto;
+		public ButtonChoosePhotoClicker (int whichphoto) {
+			super();
+			this.whichphoto = whichphoto;
+		}
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			// TODO Auto-generated method stub
+			Intent photoPickerIntent = new Intent(Intent.ACTION_PICK,
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			photoPickerIntent.setType("image/*");
+			switch (this.whichphoto) {
+			case 1: 
+				startActivityForResult(photoPickerIntent, PICK_FIRST_IMAGE_ACTIVITY_REQUESRT_CODE);
+			case 2:
+				startActivityForResult(photoPickerIntent,PICK_SECOND_IMAGE_ACTIVITY_REQUESRT_CODE);
+			}
 
-    class buttonTakePhotoClicker implements Button.OnClickListener {
-        public void onClick(View v) {
-            ContentValues values = new ContentValues();
-            values.put(Media.TITLE, fileName);
-            values.put(Media.DESCRIPTION, "Image capture by camera, muzhi");
-            Uri uri = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, values);
+		}
+	}
 
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
+	class ButtonTakePhotoClicker implements OnClickListener {
+		int whichphoto;
+		public ButtonTakePhotoClicker (int whichphoto) {
+			super();
+			this.whichphoto = whichphoto;
+		}
+		@Override
+		public void onClick(DialogInterface arg0, int arg1) {
+			ContentValues values = new ContentValues();
+			switch (this.whichphoto) {
+			case 1:
+				values.put(Media.TITLE, FIRST_FILE_NAME);
+				values.put(Media.DESCRIPTION, "First image captured by the camera, muzhi");
+			case 2:
+				values.put(Media.TITLE, SECOND_FILE_NAME);
+				values.put(Media.DESCRIPTION, "Second image captured by the camera, muzhi");
+			}
+			Uri uri = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, values);
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+			intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+			switch (this.whichphoto) {
+			case 1:
+				startActivityForResult(intent, CAPTURE_FIRST_IMAGE_ACTIVITY_REQUEST_CODE);
+			case 2:
+				startActivityForResult(intent, CAPTURE_SECOND_IMAGE_ACTIVITY_REQUEST_CODE);
+			}
+		}
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Toast toast = Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT);
-        toast.show();
-        setContentView(R.layout.camera);
-        buttonTakePhoto = (Button) findViewById(R.id.buttonTakePhoto);
-        buttonChoosePhoto = (Button) findViewById(R.id.buttonChoosePhoto);
-        imgTakenPhoto = (ImageView) findViewById(R.id.imageView);
+	class ButtonPhotoClicker implements Button.OnClickListener {
+		int whichphoto;
 
-        buttonTakePhoto.setOnClickListener(new buttonTakePhotoClicker());
-        buttonChoosePhoto.setOnClickListener(new buttonChoosePhotoClicker());
-    }
+		public ButtonPhotoClicker(int whichphoto) {
+			super();
+			this.whichphoto = whichphoto;
+		}
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Toast.makeText(this, "on activity result is called", Toast.LENGTH_SHORT).show();
-        switch (requestCode) {
-        case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
-            if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "photo taken ok", Toast.LENGTH_SHORT).show();
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT).show();
-            }
-            break;
+		@Override
+		public void onClick(View v) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(CameraActivity.this);
+			builder.setMessage("选择相册照片或拍照上传")
+			.setCancelable(true)
+			.setPositiveButton("拍照上传",
+					new ButtonTakePhotoClicker(this.whichphoto)
+					)
+					.setNegativeButton("相册选择",
+							new ButtonChoosePhotoClicker(this.whichphoto)
+							);
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
 
-        case PICK_IMAGE_ACTIVITY_REQUESRT_CODE:
-            if ((resultCode == RESULT_OK) && (data.getData() != null)) {
-                // resize the image size to 750 px (either width or height,
-                // whichever comes first)
-                Bitmap bitmap;
-                try {
-                    bitmap = Util.decodeFile(new File(new URI(data.getDataString())), 750);
-                    Log.i("photo size is ", "" + bitmap.getWidth() + " , " + bitmap.getHeight());
-                    imgTakenPhoto.setImageBitmap(bitmap);
-                } catch (URISyntaxException e) {
-                    Toast.makeText(this, "Unable to locate or decode the image you selected.",
-                            Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(this, "Image not been selected!", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Toast toast = Toast.makeText(getApplicationContext(), "onCreate",
+				Toast.LENGTH_SHORT);
+		toast.show();
+		setContentView(R.layout.camera);
+//		buttonFirstPhoto = (Button) findViewById(R.id.firstPhotoButton);
+//		buttonSecondPhoto = (Button) findViewById(R.id.secondPhotoButton);
+	
+//		buttonFirstPhoto.setOnClickListener(new ButtonPhotoClicker(1));
+//		buttonSecondPhoto.setOnClickListener(new ButtonPhotoClicker(2));
+		
+		firstPhoto = (ImageView) findViewById(R.id.imageView1);
+		secondPhoto = (ImageView) findViewById(R.id.imageView2);
+		firstPhoto.setClickable(true);
+		secondPhoto.setClickable(true);
+		firstPhoto.setOnClickListener(new ButtonPhotoClicker(1));
+		secondPhoto.setOnClickListener(new ButtonPhotoClicker(2));	
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Toast.makeText(this, "on activity result is called", Toast.LENGTH_SHORT)
+		.show();
+		switch (requestCode) {
+		case CAPTURE_FIRST_IMAGE_ACTIVITY_REQUEST_CODE:
+			if (resultCode == RESULT_OK) {
+				Toast.makeText(this, "photo taken ok", Toast.LENGTH_SHORT)
+				.show();
+			} else if (resultCode == RESULT_CANCELED) {
+				Toast.makeText(this, "Picture was not taken: result_canceled",
+						Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(this, "Picture was not taken: else",
+						Toast.LENGTH_SHORT).show();
+			}
+			break;
+		case CAPTURE_SECOND_IMAGE_ACTIVITY_REQUEST_CODE:
+			if (resultCode == RESULT_OK) {
+
+			} else if (resultCode == RESULT_CANCELED) {
+
+			} else {
+
+			}
+
+		case PICK_FIRST_IMAGE_ACTIVITY_REQUESRT_CODE:
+			if ((resultCode == RESULT_OK) && (data.getData() != null)) {
+				// resize the image size to 750 px (either width or height,
+				// whichever comes first)
+				Bitmap bitmap;
+				try {
+					bitmap = Util.decodeFile(
+							new File(new URI(data.getDataString())), 750);
+					Log.i("photo size is ", "" + bitmap.getWidth() + " , "
+							+ bitmap.getHeight());
+					firstPhoto.setImageBitmap(bitmap);
+				} catch (URISyntaxException e) {
+					Toast.makeText(
+							this,
+							"Unable to locate or decode the image you selected.",
+							Toast.LENGTH_SHORT).show();
+				}
+			} else {
+				Toast.makeText(this, "Image not been selected!",
+						Toast.LENGTH_SHORT).show();
+			}
+		case PICK_SECOND_IMAGE_ACTIVITY_REQUESRT_CODE:
+			if ((resultCode == RESULT_OK) && (data.getData() != null)) {
+				// resize the image size to 750 px (either width or height,
+				// whichever comes first)
+				Bitmap bitmap;
+				try {
+					bitmap = Util.decodeFile(
+							new File(new URI(data.getDataString())), 750);
+					Log.i("photo size is ", "" + bitmap.getWidth() + " , "
+							+ bitmap.getHeight());
+					secondPhoto.setImageBitmap(bitmap);
+				} catch (URISyntaxException e) {
+					Toast.makeText(
+							this,
+							"Unable to locate or decode the image you selected.",
+							Toast.LENGTH_SHORT).show();
+				}
+			} else {
+				Toast.makeText(this, "Image not been selected!",
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
 }
